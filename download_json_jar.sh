dcc43f86bbe460e32b196171be2b61ca9427b16d
Rikard Fröberg <rikard@morus.se>
2018-02-08 16:51:57 +0100

    Script to fetch the org.json.jar

--- download_json_jar.sh
+++ download_json_jar.sh
@@ -4,4 +4,4 @@ DEST_DIR=lib
 mkdir -p ${DEST_DIR}
 
 echo "Downloading Json-lib (Apache License) from maven..."
-wget 'https://search.maven.org/remotecontent?filepath=org/json/json/20171018/json-20171018.jar' -O ${DEST_DIR}/org.json.jar
+wget --no-check-certificate 'https://search.maven.org/remotecontent?filepath=org/json/json/20171018/json-20171018.jar' -O ${DEST_DIR}/org.json.jar
