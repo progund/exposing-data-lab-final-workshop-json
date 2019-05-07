7161c3ff834fcb89885030e8f8fc0e521ff90047
Rikard Fröberg <rikard@morus.se>
2019-05-07 14:01:01 +0200

    adds user-agent spoof - needed since they changed something

--- solution/refactored/se/itu/brewdog/main/BrewdogBeerLookup.java
+++ solution/refactored/se/itu/brewdog/main/BrewdogBeerLookup.java
@@ -9,6 +9,8 @@ public class BrewdogBeerLookup {
   
   public static void main(String[] args) {
 
+    System.setProperty("http.agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+    
     String query = "";
     if (args.length != 0) {
       query = args[0];
--- solution/se/itu/brewdog/main/FetchJson.java
+++ solution/se/itu/brewdog/main/FetchJson.java
@@ -20,7 +20,8 @@ public class FetchJson {
   private static final String QUERY = "beers?page=1&per_page=10";
   
   public static void main(String[] args) {
-    
+
+    System.setProperty("http.agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
     System.out.println("Connecting to the punk api");
     System.out.println("Using this url: " + END_POINT + QUERY);
 
